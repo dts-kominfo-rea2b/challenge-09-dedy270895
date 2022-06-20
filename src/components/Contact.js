e666195a11cd842bccc69ec048f91164dd0c57cd
dedy270895 <dedospratamos@gmail.com>
2022-06-20 15:11:42 +0700

    menambahkan component contact dan header serta css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = ({data}) => {
     return (
-        <></>
+        <div className='contact'>
+            <br></br>
+            <img src={data.photo} className="imgProfile" alt='gambarprofile'></img>
+            <div className='item'>
+                <h3>{data.name}</h3>
+                <h3>{data.phone}</h3>
+                <h3>{data.email}</h3>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
+
+    
     return (
-        <></>
+        <div>
+            <div className='header'>
+                <h1>Call a Friend</h1>
+                <h3>your friendly contact app</h3>
+            </div>
+            <div>
+            <hr className='hr'></hr>
+            </div>
+        </div>
+
     )
 }
 
